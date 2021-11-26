d4425fc61460875383399f6c7f87efde54fe5500
Imed Ramdani <a.forum@hotmail.fr>
2021-11-26 19:04:13 +0100

    Updates

---
+++ example/precache-manifest.b3fae25293f5f5b842ccfe44268e8b03.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40f0a720fa6fadf590adb368fce52ce7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "883501087e8e55f67814",
+    "url": "./static/css/main.b67d50bf.chunk.css"
+  },
+  {
+    "revision": "b130533fb115f70d87b8",
+    "url": "./static/js/2.3316fc19.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.3316fc19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "883501087e8e55f67814",
+    "url": "./static/js/main.92e7b5d4.chunk.js"
+  },
+  {
+    "revision": "6fce53c7c7713ebf61712cc2929746fa",
+    "url": "./static/js/main.92e7b5d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55620ce3af4cdff00448",
+    "url": "./static/js/runtime-main.a3046699.js"
+  }
+]);
